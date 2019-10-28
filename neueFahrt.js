cdaeaa362da8ba78b993ec47d2d27a08f745ad9c
Hannes <hannes.schaetzle@gmx.de>
2019-10-28 12:59:49 +0100

    erste Versuche Navigation

---
+++ neueFahrt.js
@@ -0,0 +1,15 @@
+class neueFahrt {
+  constructor(app) {
+    this._app = app;
+  }
+async show(){
+    console.log("Neue Fahrt klappt");
+    let html = await fetch("neueFahrt.html");
+    let htmlContent ="";
+
+    if(html.ok){
+      htmlContent = await html.text();
+    }
+    this._app.setPageContent(htmlContent);
+  }
+}

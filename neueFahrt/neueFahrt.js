258598036344798d5f6efd4ecd86049a477010cd
D074097 <54674129+D074097@users.noreply.github.com>
2019-10-29 09:31:09 +0100

    Neue Fahrt
    Css und JS bearbeitet, seite schicker und felder auslesbar

--- neueFahrt/neueFahrt.js
+++ neueFahrt/neueFahrt.js
@@ -13,3 +13,52 @@ async show(){
     this._app.setPageContent(htmlContent);
   }
 }
+window.addEventListener("load", () =>{
+let selectFahrer = document.getElementById("selectFahrer");
+let fahrerInput = null;
+let selectMitfahrer = document.getElementById("selectMitfahrer");
+let mitfahrerInput = null;
+let inputDistanz = document.getElementById("inputDistanz");
+let inputVon = document.getElementById("inputVon");
+let inputNach = document.getElementById("inputNach");
+let inputPreis = document.getElementById("inputPreis");
+let inputDatum = document.getElementById("inputDatum");
+let inputNotiz = document.getElementById("inputNotiz");
+let erstellenButton = document.getElementById("butErstellen");
+let dataArray = [];
+
+erstellenButton.addEventListener("click", () => {
+    console.log("Funkt");
+    inputDistanz = inputDistanz.value;
+    inputVon = inputVon.value;
+    inputNach = inputNach.value;
+    inputPreis = inputPreis.value;
+    inputDatum = inputDatum.value;
+    dataArray.push(inputDistanz, inputVon, inputNach, inputPreis, inputDatum);
+    inputNotiz.innerHTML = dataArray;
+});
+
+selectFahrer.addEventListener("change", () =>{
+  console.log("Change now");
+  console.log(selectFahrer.value);
+  dataArray.push(selectFahrer.value);
+});
+
+selectMitfahrer.addEventListener("change", () =>{
+  console.log("Change now");
+  console.log(selectMitfahrer.value);
+  dataArray.push(selectMitfahrer.value);
+});
+
+let hinzuBut = document.getElementById("buttonHinzu");
+
+hinzuBut.addEventListener("click", function(){
+  console.log("Till stinkt");
+  let buttonDivBox = document.getElementById("divButtonHinzu");
+  let neuerButton = document.createElement("neuerBut");
+  let data = document.createTextNode("tachchen");
+  neuerButton.appendChild(data);
+  buttonDivBox.appendChild(neuerButton);
+});
+
+});

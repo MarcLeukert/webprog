33bbc50a9629564708f3111d673bbdf3452ab89e
MarcLeukert <marc.leukert@web.de>
2019-10-30 12:06:12 +0100

    erstellen der class verbindlichkeiten und forderungen

---
+++ forderungen.js
@@ -0,0 +1,11 @@
+class Forderungen{
+
+  constructor(forderungID, inhaberID, schuldnerID, fahrtID, summe){
+    this.forderungID = forderungID;
+    this._fahrtID = fahrtID;
+    this._inhaberID = inhaberID;
+    this._schuldnerID = schuldnerID;
+    this._summe = summe;
+
+  }
+}
---
+++ verbindlichkeiten.js
@@ -0,0 +1,9 @@
+class Verbindlichkeiten{
+  constructor(verbindlichkeitID, inhaberId, gläubigerId, fahrtId, summe ){
+    this._verbindlichkeitID = verbindlichkeitID;
+    this._fahrtID = fahrtID;
+    this.glaeubigerID = glaeubigerID;
+    this._inhaberID = schuldnerID;
+    this._summe = summe;
+  }
+}

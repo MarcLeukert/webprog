9008d8b056def1b0fd9fd99d9032e5252439f749
Hannes <hannes.schaetzle@gmx.de>
2019-11-05 13:20:35 +0100

    UUID implemented

--- Konto/user.js
+++ Konto/user.js
@@ -1,6 +1,6 @@
 class User {
-  constructor(userID, vorname, nachname) {
-    this.userID = userID;
+  constructor(vorname, nachname) {
+    this.userID = this.create_UUID();
     this.vorname = vorname;
     this.nachname = nachname;
     this.amountForderungen = [];
@@ -37,6 +37,17 @@ class User {
   getForderungen(){
     return this.amountForderungen;
   }
+
+
+  create_UUID(){
+        var dt = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (dt + Math.random()*16)%16 | 0;
+            dt = Math.floor(dt/16);
+            return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    }
 
   getUserID(){
     return this.userID;

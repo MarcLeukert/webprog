32da279213e019c35174e7cf635e39d0a9e30263
Hannes <hannes.schaetzle@gmx.de>
2019-11-05 13:23:45 +0100

    UUIDs
    verbindlichkeiten.js
    forderungen.js

--- forderungen.js
+++ forderungen.js
@@ -1,11 +1,21 @@
 class Forderung{
 
-  constructor(forderungsID, inhaberID, schuldnerID, fahrtID, wert){
-    this.forderungID = forderungsID;
+  constructor(inhaberID, schuldnerID, fahrtID, wert){
+    this.forderungID = this.create_UUID();
     this._fahrtID = fahrtID;
     this._inhaberID = inhaberID;
     this._schuldnerID = schuldnerID;
     this.wert = wert;
 
   }
+
+  create_UUID(){
+        var dt = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (dt + Math.random()*16)%16 | 0;
+            dt = Math.floor(dt/16);
+            return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    }
 }
